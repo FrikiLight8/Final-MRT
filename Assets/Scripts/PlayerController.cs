f2b6f74bdb488f6b5761a8a6bc98cbcb9f19f228
FrikiLight8 <91629600+FrikiLight8@users.noreply.github.com>
2024-12-02 19:55:38 -0300

    Añadido Mecánica de poner Bombas

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
     public float speed = 5f;
     private Rigidbody rb;
+    [SerializeField]
+    public GameObject bombPrefab; // Prefab de la bomba
+    public float bombCooldown = 2f; // Tiempo de espera entre colocar bombas
+    private float lastBombTime = -Mathf.Infinity; // Última vez que se colocó una bomba
 
     void Start()
     {
@@ -28,6 +34,28 @@ public class PlayerController : MonoBehaviour
 
         // Aplicar el movimiento
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+        // Detectar si se presiona la tecla para colocar una bomba
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= lastBombTime + bombCooldown)
+        {
+            PlaceBomb();
+        }
+    }
+
+    void PlaceBomb()
+    {
+        // Redondear la posición del jugador para ajustarse a la cuadrícula
+        Vector3 bombPosition = new Vector3(
+            Mathf.Round(transform.position.x),
+            transform.position.y,
+            Mathf.Round(transform.position.z)
+        );
+
+        // Instanciar la bomba en la posición ajustada
+        Instantiate(bombPrefab, bombPosition, Quaternion.identity);
+
+        // Actualizar el tiempo de colocación de la última bomba
+        lastBombTime = Time.time;
     }
 }
 
